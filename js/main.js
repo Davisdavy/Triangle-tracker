c143e81e99ae5b2b15aec040592bc124ec344e58
Davisdavy <davisdvay96@gmail.com>
2019-06-01 07:57:16 +0300

    Add an answer input

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+
+
+    function logicCheck(){
+        var side1 = parseInt(document.getElementById("plot1").value);
+        var side2 =parseInt(document.getElementById("plot2").value);
+        var side3 = parseInt(document.getElementById("plot3").value);
+        
+    if(side1==side2 && side2==side3) 
+    {
+        /* If all sides are equal */
+        document.getElementById("Result").innerHTML="Equilateral triangle.";
+    }
+    else if(side1==side2 || side1==side3 || side2==side3) 
+    {
+        /* If any two sides are equal */
+        document.getElementById("Result").innerHTML="Isosceles triangle.";
+    }
+    else 
+    {
+        /* If none sides are equal */
+        document.getElementById("Result").innerHTML="Scalene triangle.";
+    }
+}
