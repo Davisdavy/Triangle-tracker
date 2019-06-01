f91aa3d88407f987ac479468cc9d61d238cb120a
Davisdavy <davisdvay96@gmail.com>
2019-06-01 08:42:38 +0300

    Add an array constructor for the nick name add and display, add a nickname section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 
-
+///**********LOGIC FUNCTION************* */
     function logicCheck(){
         var side1 = parseInt(document.getElementById("plot1").value);
         var side2 =parseInt(document.getElementById("plot2").value);
@@ -9,19 +9,41 @@
     if(side1==side2 && side2==side3) 
     {
         /* If all sides are equal */
-       var element = document.getElementById("Result");
+       document.getElementById("Result");
        element.innerHTML="Equilateral triangle.";
     }
     else if(side1==side2 || side1==side3 || side2==side3) 
     {
         /* If any two sides are equal */
-       var element= document.getElementById("Result");
+       document.getElementById("Result");
        element.innerHTML="Isosceles triangle.";
     }
     else 
     {
         /* If none sides are equal */
-       var element= document.getElementById("Result");
+       document.getElementById("Result");
        element.innerHTML="Scalene triangle.";
     }
+}
+/////////**************ADD NAME FUNCTION****************** */
+var x = 0;
+var array = Array();
+
+function add_element_to_array()
+{
+ array[x] = document.getElementById("text1").value;
+ alert("Hello " + array[x]);
+ x++;
+ document.getElementById("text1").value = "";
+}
+
+function display_array()
+{
+   var e = "<hr/>";   
+    
+   for (var y=0; y<array.length; y++)
+   {
+     e += "Nickname " + y + " = " + array[y] + "<br/>";
+   }
+   document.getElementById("next").innerHTML = e;
 }
